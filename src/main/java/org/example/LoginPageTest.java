457c1939ea1afd8eb9ac094c78f8079df5d5a89d
M0nesz <balimn13@gmail.com>
2024-06-03 13:15:45 +0200

    Setting up file structure

---
+++ src/main/java/org/example/LoginPageTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LoginPageTest extends PlaywrightFactory{
+
+    @Test
+    void shouldClickButton() {
+        page.navigate("data:text/html,<script>var result;</script><button onclick='result=\"Clicked\"'>Go</button>");
+        page.locator("button").click();
+        assertEquals("Clicked", page.evaluate("result"));
+    }
+
+    @Test
+    void shouldCheckTheBox() {
+        page.setContent("<input id='checkbox' type='checkbox'></input>");
+        page.locator("input").check();
+        assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
+    }
+
+    @Test
+    void shouldSearchWiki() {
+        page.navigate("https://www.wikipedia.org/");
+        page.locator("input[name=\"search\"]").click();
+        page.locator("input[name=\"search\"]").fill("playwright");
+        page.locator("input[name=\"search\"]").press("Enter");
+        assertEquals("https://en.wikipedia.org/wiki/Playwright", page.url());
+    }
+}
